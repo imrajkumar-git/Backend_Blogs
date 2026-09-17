6dbac826cab331833459485ae38dc58b0d8f74ba
Rajkumar <rajkumararyal0977@gmail.com>
2026-09-18 00:13:43 +0400

    all added

--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG =  os.getenv("DEBUG", "True") == "True"
-FRONTEND_URL = config("FRONTEND_URL", default="http://localhost:3000")
+FRONTEND_URL = config("FRONTEND_URL", default="https://full-stack-django-nextjs-otpwithjwt.vercel.app")
 # Base URL used when building links inside emails (e.g. a "click to verify"
 # link, if you add one later). Falls back to FRONTEND_URL if not set.
 EMAIL_PAGE_DOMAIN = config("EMAIL_PAGE_DOMAIN", default=FRONTEND_URL)
